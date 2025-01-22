d98c9840a10e57272d81870b7b5e8652b9708ad4
Saurabh <saurabh@lambdatest.com>
2025-01-22 17:24:02 +0530

    Changed the name

--- SDK_CLI/parallelScript.sh
+++ SDK_CLI/parallelScript.sh
@@ -2,7 +2,7 @@ for i in {1..10}; do
     FIRST_DIGIT=$((3 + i))
     PORT="${FIRST_DIGIT}000"
     echo $PORT
-    INSTANCE_ID=$i npx smartui exec node SDK_CLI/SmartUINode.js --port $PORT & 
+    INSTANCE_ID=$i npx smartui exec node SDK_CLI/parallelTesting.js --port $PORT & 
 done
 wait
 
