bcb78016e0ec27ed5110e0abbfd11b8371ee2fbf
Saurabh <saurabh@lambdatest.com>
2025-01-23 13:32:06 +0530

    changed the value to dynamic

--- SDK_CLI/parallelScript.sh
+++ SDK_CLI/parallelScript.sh
@@ -1,10 +1,10 @@
-#!/bin/bash   # Add this line at the top if you specifically need bash
+# Get count from environment variable, default to 30 if not set
+COUNT=${LOOP_COUNT:-30}
 
-# Option 1: Using seq
-for i in $(seq 1 30); do
+for i in $(seq 1 $COUNT); do
     FIRST_DIGIT=$((3 + i))
     PORT="${FIRST_DIGIT}000"
     echo $PORT
-    INSTANCE_ID=$i npx smartui exec node SDK_CLI/parallelTesting.js --port $PORT &
+    INSTANCE_ID=$i npx smartui exec node SeleniumTest.js --config smartui-web.json --port $PORT & 
 done
 wait
